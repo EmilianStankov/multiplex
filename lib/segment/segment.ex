21c900d4fcdb47790bf9493b2d16b4398a6bc1a9
Emilian Stankov <eminstine@gmail.com>
2019-06-16 22:49:02 +0300

    Move segments to the segments directory

--- lib/segment/segment.ex
+++ lib/segment/segment.ex
@@ -2,11 +2,15 @@ defmodule Multiplex.Segment do
   import FFmpex
   use FFmpex.Options
 
-  def extract_segments(input_file, segment_time \\ 4) do
+  def extract_segments(input_file, file_ext \\ ".mp3", segment_time \\ 4) do
+    basename = Path.basename(input_file, file_ext)
+
+    File.mkdir_p("segments/#{basename}")
+
     command =
       FFmpex.new_command()
       |> add_input_file(input_file)
-      |> add_output_file("segments/#{input_file}.%3d.ts")
+      |> add_output_file("segments/#{basename}/#{basename}.%3d.ts")
       |> add_file_option(option_f("segment"))
       |> add_file_option(%FFmpex.Option{name: "-segment_time", argument: segment_time})
 
